a24985f050b64ff5ce1491b2cc8e3b05edd5fa16
“Kunalshakya” <kunalshakya213@gmail.com>
2023-04-29 11:32:52 +0530

    code refactor and update README

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
-var button = document.getElementById("menu");
-var mobileNav = document.getElementById("mobileNav");
+const button = document.getElementById("menu");
+const mobileNav = document.getElementById("mobileNav");
 
 
 button.addEventListener("click", () =>{
     if(mobileNav.style.display === "none"){
         mobileNav.style.display = "block";
         button.style.opacity = 0.5;
-        console.log("block")
     }
     else{
         mobileNav.style.display = "none";
         button.style.opacity = 1;
-        console.log("none")
     }
 })
